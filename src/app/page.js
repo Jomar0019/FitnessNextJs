1f1a969e82f7dfc408fafaf77107dff4d9dd256d
Jomar0019 <jomarmagpoot24@gmail.com>
2024-07-04 23:47:45 +0800

    Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,31 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-red-300 flex justify-center items-center">
-      <h1 className="text-[72px] text-white font-extrabold">Home</h1>
+    <div className="w-full h-[90vh] flex justify-center items-center bg-[#f5f5f5]">
+      <div className="w-[1600px]">
+        <h4 className="font-semibold text-md text-[#222222] uppercase py-6">the best fitness club in town</h4>
+        <div className="w-full h-[12] flex gap-4">
+          <div className="basis-8/12 ">
+            <h1 className="text-[110px] uppercase font-bold text-[#222222]">Transforming lives through fitness</h1>
+            <div className="py-6 gap-6 flex">
+              <button className="bg-[#e84a5f] text-white px-6 py-3 rounded-full">Be a Member</button>
+              <button className="border border-[#e84a5f] px-6 py-3 rounded-full">Learn More</button>
+            </div>
+          </div>
+          <div className="basis-4/12 flex gap-4 items-end justify-center">
+            <div className="relative w-full h-full">
+              <Image
+                src="https://images.pexels.com/photos/2261477/pexels-photo-2261477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="home image"
+                fill
+                sizes="100%"
+                className="object-cover rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 const Navbar = () => {
   return (
-    <div className="w-full h-[80px] flex justify-center items-center p-4 shadow-md">
-      <div className="w-[1400px] flex justify-between items-center">
+    <div className="w-full h-[80px] bg-[#f5f5f5] flex justify-center items-center p-4 shadow-md">
+      <div className="w-[1600px] flex justify-between items-center">
         <div>
           <h1 className="text-[34px] text-[#e84a5f] font-bold">Flex</h1>
         </div>
         <div className="flex gap-x-8">
-          <a href="/" className="tracking-wider">Home</a>
-          <a href="/" className="tracking-wider">Program</a>
-          <a href="/" className="tracking-wider">Blog</a>
-          <a href="/" className="tracking-wider">Contact</a>
+          <a href="/" className="text-[#222222]">Home</a>
+          <a href="/" className="text-[#222222]">Program</a>
+          <a href="/" className="text-[#222222]">Blog</a>
+          <a href="/" className="text-[#222222]">Contact</a>
         </div>
         <div>
           <button className=" bg-[#e84a5f] px-6 py-2 rounded-full text-white">Get Started</button>
